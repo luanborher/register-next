f253b0b72f446e5aa134893291a50f3f2734607a
luanborher <luan.borher@gmail.com>
2023-12-14 18:15:00 -0300

    fix: eslint

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,9 @@
-import Header from "@/components/Header/Header";
+import Header from '@/components/Header/Header';
 
-export default function IndexPage() {
-  return (
-    <main className="flex flex-col gap-2">
-      <Header
-        title="Olá, Administrador!"
-        subtitle="Painel de Controle"
-        profile
-      />
-    </main>
-  );
-}
+const IndexPage = () => (
+  <main className="flex flex-col gap-2">
+    <Header title="Olá, Administrador!" subtitle="Painel de Controle" profile />
+  </main>
+);
+
+export default IndexPage;
